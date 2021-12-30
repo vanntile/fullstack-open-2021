44cec5948d2053a702c5c14e77980a0188fece0f
vanntile <33692650+vanntile@users.noreply.github.com>
2021-12-30 19:43:17 +0200

    wip: :construction: add exercise 3.10

--- part3/people-api/src/app.js
+++ part3/people-api/src/app.js
@@ -14,11 +14,19 @@ const routes = require('./routes')
 const app = express()
 
 // Middleware
-app.use(helmet())
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      'script-src': ["'self'", "'unsafe-inline'"],
+    },
+  }),
+)
 app.use(cors())
 app.use(express.json())
 morgan.token('data', (req, _) => (req.method === 'POST' ? JSON.stringify(req.body) : ''))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('build'))
 
 log.info(`Connecting to ${config.PORT}`)
 
