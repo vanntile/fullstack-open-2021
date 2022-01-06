6d1757c04f16e4818d0994c4215e914c14695444
vanntile <33692650+vanntile@users.noreply.github.com>
2022-01-06 20:20:25 +0200

    wip: :construction: add exercises 7.4 - 7.8

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor] = useField('text')
+  const [info, resetInfo] = useField('text')
   const history = useHistory()
+
+  const resetFields = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     })
 
-    props.notify({ type: 'success', message: `A new anecdote "${content}" has been created` })
+    resetFields()
+
+    props.notify({ type: 'success', message: `A new anecdote "${content.value}" has been created` })
     history.push('/')
   }
 
@@ -26,17 +35,25 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name="content" value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name="content" {...content} />
         </div>
         <div>
           author
-          <input name="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name="author" {...author} />
         </div>
         <div>
           url for more info
-          <input name="info" value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name="info" {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault()
+            resetFields()
+          }}
+        >
+          reset
+        </button>
       </form>
     </div>
   )
