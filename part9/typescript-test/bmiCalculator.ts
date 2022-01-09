4daf9bdc36012151e9b386ee8c07cb988163c92e
vanntile <33692650+vanntile@users.noreply.github.com>
2022-01-09 20:17:29 +0200

    wip: :construction: add exercises 9.1 - 9.3

--- part9/typescript-test/bmiCalculator.ts
+++ part9/typescript-test/bmiCalculator.ts
@@ -1,5 +1,33 @@
 const calculateBmi = (height: number, weight: number): string => {
-  return 'aa'
+  const bmi = weight / (height / 100) ** 2
+
+  if (bmi < 16) return 'Underweight (Severe thinness)'
+  else if (bmi < 17) return 'Underweight (Moderate thinness)'
+  else if (bmi < 18.5) return 'Underweight (Mild thinness) '
+  else if (bmi < 25) return 'Normal (healthy weight)'
+  else if (bmi < 30) return 'Overweight (Pre-obese)'
+  else if (bmi < 35) return 'Obese (Class I)'
+  else if (bmi < 40) return 'Obese (Class II)'
+  else return 'Obese (Class III)'
 }
 
-console.log(calculateBmi(180, 74))
+const parseArguments = (args: Array<string>): number[] => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 4) throw new Error('Too many arguments')
+  if (isNaN(Number(args[2])) || isNaN(Number(args[3]))) throw new Error('Provided values were not numbers!')
+
+  return [Number(args[2]), Number(args[3])]
+}
+
+try {
+  const [height, weight] = parseArguments(process.argv)
+  console.log(`BMI for ${height}cm and ${weight}kg, the result is: ${calculateBmi(height, weight)}`)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
+
+export {}
---
+++ part9/typescript-test/exerciseCalculator.ts
@@ -0,0 +1,53 @@
+interface Result {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
+}
+
+const exerciseCalculator = (record: number[], target: number): Result => {
+  const average = record.reduce((a, b) => a + b) / record.length
+  const rating = average >= target ? 3 : average >= target / 2 ? 2 : 1
+  const ratingDescription = rating === 3 ? 'Target achieved' : rating === 2 ? 'A bit more next time' : 'Target failed'
+
+  return {
+    periodLength: record.length,
+    trainingDays: record.filter((e) => e > 0).length,
+    success: average >= target,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  }
+}
+
+const parseArguments = (args: Array<string>): [number, number[]] => {
+  if (args.length < 3) throw new Error('Not enough arguments')
+  let target = 1
+  const records: number[] = []
+
+  if (isNaN(Number(args[2]))) throw new Error('Provided target is not a number')
+  else target = Number(args[2])
+  args.slice(3).forEach((e) => {
+    if (isNaN(Number(e))) throw new Error('Provided values were not numbers!')
+    else records.push(Number(e))
+  })
+
+  return [target, records]
+}
+
+try {
+  const [target, records] = parseArguments(process.argv)
+  console.log(exerciseCalculator(records, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
+
+export {}
